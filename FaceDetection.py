5691206a7a50129dd55e1168bb648c49c737fe46
sun000 <sun000mail@gmail.com>
2019-09-15 21:31:18 +0800

    fix the bugs about evaluate F and D, add Face-Recognizer's predict funs

--- FaceDetection.py
+++ FaceDetection.py
@@ -13,10 +13,25 @@ class FaceRecognizer(object):
             adaBoost = AdaBoost(self.ctx)
             adaBoost.train()
             self.adaBoost_models.append(adaBoost)
-            break
+            #print(self.ctx.valid_predict)
+            self.ctx.update_valid_features()
+            #print(len(self.ctx.valid_p_features[0]))
+            #print(len(self.ctx.valid_n_features[0]))
 
-    def predict(self):
-        pass
+    def predict(self, test_images):
+        predict_positive_idx = list(range(len(test_images)))
+        for adaBoost in self.adaBoost_models:
+            test_predict = adaBoost.predict(test_images)
+            for idx in range(len(test_predict) -1 , -1, -1):
+                if test_predict[idx] < 1.0 - EPS:
+                    del test_images[idx]
+                    del predict_positive_idx[idx]
+        predict_labels = []
+        for idx in predict_positive_idx:
+            while(len(predict_labels)) < idx:
+                predict_labels.append(0)
+            predict_labels.append(1)
+        return predict_labels
 
 def FaceDecetive(object):
     def __init__(self):
@@ -27,6 +42,10 @@ def main():
     ctx = get_debug_context()
     faceRecognizer = FaceRecognizer(ctx)
     faceRecognizer.train()
+    test_data = []
+    #for i in range(100):
+        #test_data.append(get_random_sample((24, 24)))
+    print(faceRecognizer.predict(ctx.train_p_data + ctx.train_n_data))
 
 def get_random_sample(size):
     sample = np.random.normal(size=size)
--- context.py
+++ context.py
@@ -5,6 +5,7 @@ from HaarLikeFeature import get_all_feature_extractor
 from IntegralImage import to_integral_image
 from functools import partial
 import numpy as np
+from utils import EPS
 
 class Context(object):
     def __init__(self,
@@ -20,25 +21,30 @@ class Context(object):
         self.F, self.D = 1.0, 1.0
         self.F_target = F_target
         self.image_size = image_size
+
         # 加载数据并提取特征
         print("Load data......")
         self.load_data(train_p_data_dir, train_n_data_dir, valid_p_data_dir, valid_n_data_dir)
+        self.valid_predict = []
         self.features_extractors = get_all_feature_extractor(image_size)
         print("Extract features......")
         self.extract_features()
         print("Finish extecting features!")
 
     def load_data(self, train_p_data_dir, train_n_data_dir, valid_p_data_dir, valid_n_data_dir):
-        self.train_p_data = read_images(train_p_data_dir, self.image_size, normalize=True, limit=30)
-        self.train_n_data = read_images(train_n_data_dir, self.image_size, normalize=True, limit=30)
-        self.valid_p_data = read_images(valid_p_data_dir, self.image_size, normalize=True, limit=10)
-        self.valid_n_data = read_images(valid_n_data_dir, self.image_size, normalize=True, limit=10)
+        self.train_p_data = read_images(train_p_data_dir, self.image_size, normalize=True, limit=-1)
+        self.train_n_data = read_images(train_n_data_dir, self.image_size, normalize=True, limit=-1)
+        self.valid_p_data = read_images(valid_p_data_dir, self.image_size, normalize=True, limit=70)
+        self.valid_n_data = read_images(valid_n_data_dir, self.image_size, normalize=True, limit=70)
+        self.valid_p_num, self.valid_n_num = len(self.valid_p_data), len(self.valid_n_data)
 
-    def get_features_from_images(self, images):
+    def get_features_from_images(self, images, features_extractors=None):
+        if features_extractors is None:
+            features_extractors = self.features_extractors
         with poolContext(processes=16) as pool:
             int_images = pool.map(to_integral_image, images)
         with poolContext(processes=16) as pool:
-            features = pool.map(partial(get_features, int_imgs=int_images), self.features_extractors)
+            features = pool.map(partial(get_features, int_imgs=int_images), features_extractors)
         return features
 
     def extract_features(self):
@@ -54,8 +60,28 @@ class Context(object):
         tmp_features = zip(self.valid_p_features, self.valid_n_features)
         with poolContext(processes=16) as pool:
             valid_features = pool.map(np.concatenate, tmp_features)
-        labels = np.array([1] * len(self.valid_p_data) + [0] * len(self.valid_n_data))
+        labels = np.array([1] * len(self.valid_p_features[0]) + [0] * len(self.valid_n_features[0]))
         return valid_features, labels
+
+    def update_valid_features(self): # 根据预测值更新验证集合，去除正样本中被预测为false的样本，负样本只保留fp样本
+        real_p_num = len(self.valid_p_features[0])
+        real_n_num = len(self.valid_n_features[0])
+        assert real_p_num + real_n_num == len(self.valid_predict)
+
+        p_need_delete_idx = []
+        n_need_delete_idx = []
+        for idx in range(real_p_num):
+            if self.valid_predict[idx] < EPS:
+                p_need_delete_idx.append(idx)
+        for idx in range(real_n_num):
+            if self.valid_predict[real_p_num + idx] < EPS:
+                n_need_delete_idx.append(idx)
+        with poolContext(processes=4) as pool:
+            self.valid_p_features = pool.map(partial(np.delete, obj=p_need_delete_idx), self.valid_p_features)
+        with poolContext(processes=4) as pool:
+            self.valid_n_features = pool.map(partial(np.delete, obj=n_need_delete_idx), self.valid_n_features)
+
+        self.valid_n_num = len(self.valid_n_features[0].shape) # 正样本不需要更新
 
 def get_debug_context():
     ctx = Context(0.3, 0.9, 0.1,
